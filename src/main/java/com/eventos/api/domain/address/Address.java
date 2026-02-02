0d51d0603d0f1f7a8bd7a9bcd316a297aa7c8ef6
Julyana Gusmão <julyanagcneiva@gmail.com>
2026-02-02 17:35:03 -0300

    feat: creating entities

---
+++ src/main/java/com/eventos/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventos.api.domain.address;
+
+import java.util.UUID;
+
+import com.eventos.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name = "address")
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
