6a6bda42c65ead1c6ef76bbbc7aa0a0254bb1e92
Julyana Gusmão <julyanagcneiva@gmail.com>
2026-02-03 12:33:37 -0300

    feat: create entities repositories and organize pom

--- src/main/java/com/eventos/api/repositories/CouponRepository.java
+++ src/main/java/com/eventos/api/repositories/CouponRepository.java
@@ -1,5 +1,7 @@
 package com.eventos.api.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eventos.api.domain.coupon.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
-
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, LocalDateTime currentDate);
 }
--- src/main/java/com/eventos/api/repositories/EventRepository.java
+++ src/main/java/com/eventos/api/repositories/EventRepository.java
@@ -1,11 +1,42 @@
 package com.eventos.api.repositories;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eventos.api.domain.event.Event;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+  @Query("""
+          SELECT e FROM Event e
+          LEFT JOIN FETCH e.address a
+          WHERE e.date >= :currentDate
+      """)
+  Page<Event> findUpcomingEvents(
+      @Param("currentDate") LocalDateTime currentDate,
+      Pageable pageable);
+
+  @Query("""
+      SELECT e
+      FROM Event e
+      LEFT JOIN e.address a
+      WHERE (:title = '' OR e.title LIKE CONCAT('%', :title, '%'))
+        AND (:city = '' OR a.city LIKE CONCAT('%', :city, '%'))
+        AND (:uf = '' OR a.uf LIKE CONCAT('%', :uf, '%'))
+        AND (e.date >= :startDate AND e.date <= :endDate)
+      """)
+  Page<Event> findFilteredEvents(
+      @Param("title") String title,
+      @Param("city") String city,
+      @Param("uf") String uf,
+      @Param("startDate") LocalDateTime startDate,
+      @Param("endDate") LocalDateTime endDate,
+      Pageable pageable);
 
 }
